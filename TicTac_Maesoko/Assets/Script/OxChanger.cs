0e988e926d00eff92d654c02348897db116319d6
maesoko <maesoko@jupit.co.jp>
2016-06-17 18:33:47 +0900

    [add] ボードがクリックされた際にスプライトを変更する処理を実装

---
+++ TicTac_Maesoko/Assets/Script/OxChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OxChanger : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+
+	public Sprite Ox {
+		get { return this.spriteRenderer.sprite; }
+		set { this.spriteRenderer.sprite = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
