5e16269b7b8ccbdb8afce166f2083b5e4e9aa260
maesoko <maesoko@jupit.co.jp>
2016-06-22 19:04:30 +0900

    [modify] 現在のターン記号を表示する処理を実装

--- TicTac_Maesoko/Assets/Script/BoardManager.cs
+++ TicTac_Maesoko/Assets/Script/BoardManager.cs
@@ -17,6 +17,7 @@ public class BoardManager : MonoBehaviour {
 	public GameObject player1Win;
 	public GameObject player2Win;
 	public GameObject draw;
+	public MarkChanger markChanger;
 
 	public bool IsPlayer1Turn
 	{
@@ -33,6 +34,7 @@ public class BoardManager : MonoBehaviour {
 	public void InvertTurn()
 	{
 		IsPlayer1Turn = !IsPlayer1Turn;
+		markChanger.ChangeTurnMark (IsPlayer1Turn);
 	}
 
 	void Start()
